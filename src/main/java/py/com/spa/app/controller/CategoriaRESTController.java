1fc9bfe5d11dc8657ba48699481232b1826626a6
elias.ocampo <eljo1391@gmail.com>
2021-02-08 16:21:56 -0300

    ajustes cors

--- src/main/java/py/com/spa/app/controller/CategoriaRESTController.java
+++ src/main/java/py/com/spa/app/controller/CategoriaRESTController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import py.com.spa.app.util.Utileria;
 
 @RestController
 @RequestMapping("/categoria" )
+@CrossOrigin(origins = "*")
 public class CategoriaRESTController  {
 	
 	@Autowired
