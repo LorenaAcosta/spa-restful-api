5fd5ce89b407fb2794784925391da66a5ff00cdd
Lorena Acosta <lorena.acosta95@gmail.com>
2021-02-09 23:00:07 -0300

    09/02/21

--- src/main/java/py/com/spa/app/entities/ImageModel.java
+++ src/main/java/py/com/spa/app/entities/ImageModel.java
@@ -1,32 +1,58 @@
 package py.com.spa.app.entities;
 
 
+package com.javainuse.model;
 import javax.persistence.*;
-
 @Entity
-@Table(name="image_model")
+@Table(name = "image_table")
 public class ImageModel {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue
-    private Long id;
 
-    @Column(name = "name")
-    private String name;
+  public ImageModel() {
+        super();
+    }
+   public ImageModel(String name, String type, byte[] picByte) {
 
-    @Column(name = "type")
-    private String type;
-
-    @Lob
-    @Column(name = "pic")
-    private byte[] pic;
-
-
-
-//Custom Construtor
-    public ImageModel(String name, String type, byte[] pic) {
         this.name = name;
         this.type = type;
-        this.pic = pic;
+        this.picByte = picByte;
     }
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    
+    //image bytes can have large lengths so we specify a value
+    //which is more than the default length for picByte column
+    @Column(name = "picByte", length = 1000)
+    private byte[] picByte;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+   public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getPicByte() {
+        return picByte;
+
+    }
+
+    public void setPicByte(byte[] picByte) {
+       this.picByte = picByte;
+
+    }
+
 }
