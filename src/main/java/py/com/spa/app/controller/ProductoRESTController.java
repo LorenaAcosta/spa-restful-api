cf574c643dca0925a6555fde466c89e11d0ec60b
elias.ocampo <eljo1391@gmail.com>
2021-04-13 23:36:28 -0400

    correccion producto controller

--- src/main/java/py/com/spa/app/controller/ProductoRESTController.java
+++ src/main/java/py/com/spa/app/controller/ProductoRESTController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
